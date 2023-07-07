b397f8cfbb37bb93352e19338f092ef1ed2e7155
ptchoupe <ptchoupe8@gmail.com>
2023-07-07 00:20:53 -0500

    terraform

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+my_region     = "us-east-1"
+profile       = "default"
+topic_name    = "sns-dev"
+end_point     = "abcdefe8@gmail.com"  #please replace this email by your own email
+protocol_type = "email"
+Team          = "dev"
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+
+}
+
+variable "profile" {
+  type    = string
+  default = "default"
+
+}
+
+variable "topic_name" {
+  type    = string
+  default = "sns-dev"
+
+}
+
+variable "Team" {
+  type    = string
+  default = "dev"
+
+}
+
+variable "protocol_type" {
+  type    = string
+  default = "email"
+
+}
+
+variable "end_point" {
+  type    = string
+  default = "ptchoupe8@gmail.com"
+
+
+}
